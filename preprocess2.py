1e28bc95b8829f48f06f27a2bec6a0ac70b943ab
BenceDD <mikulasbence@gmail.com>
2016-11-10 14:01:03 +0100

    GC test added

--- preprocess2.py
+++ preprocess2.py
@@ -1,26 +1,23 @@
 import csv
 import os
 import gc
-from collections import defaultdict
-
+import operator
 import numpy as np
+from collections import defaultdict
 from keras.preprocessing import image
 
 def csv_load(csv_file_name = "train_info_modified_fixed.csv"):
     csv_data = {}
     author_stat = defaultdict(int)
 
-    try:
-        with open(csv_file_name) as csvfile:
-            reader = csv.reader(csvfile)
-            next(reader, None)
-            for line in reader:
-                csv_data[line[0]] = line[1:]
-                author_stat[line[1]] += 1
-    except Exception as e:
-        print(e)
+    with open(csv_file_name) as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader, None)
+        for line in reader:
+            csv_data[line[0]] = line[1:]
+            author_stat[line[1]] += 1
 
-    return csv_data, author_stat
+    return csv_data, sorted(author_stat.items(), key=operator.itemgetter(1))
 
 
 def csv_select(sorce_csv_data, target_authors):
@@ -28,33 +25,38 @@ def csv_select(sorce_csv_data, target_authors):
 
 
 def load_images(csv_data, location = "train_sample"):
+    # TODO: limit lehetőséget hozzáadni!
     data = []
     labels = []
-    counter=0
+    
     for subdir, dirs, files in os.walk(location):
         for file in files:
             if file in csv_data:
                 img_loc = os.path.join(subdir, file)
                 img = image.load_img(img_loc, target_size=(256, 256))
                 labels.append(csv_data[file][0])
-                img_arr=np.array(img)
+                img_arr = np.array(img) # TODO: dtype="uint8" - de lehet hogy gond lesz a kerassal, pl standardizálás
                 data.append(img_arr)
                 
+                img_loc = None
                 img = None
                 img_arr = None
 
                 if len(labels) % 2000 == 0:
                     print(len(labels), "images loaded")
 
-                if (counter%10):
-                    gc_res=gc.collect()
-                    print("GC collect: ",gc_res)
-                    
-                counter=counter+1
-                
-    # gc.collect() - nem volt sok értelme
-    # byte-ként is lehetne tárolni: np.array(data, dtype="uint8") - ennek sem volt sok értelme
-    return np.array(data), np.array(labels)
+                if len(labels) % 10 == 0: # ezt meg kellene próbálni növelni akár 2000-ig
+                    print("GC collect: ", gc.collect())
+
+    np_data = np.array(data)
+    np_labels = np.array(labels)
+
+    # takarítás...
+    data = None
+    labels = None
+    gc.collect()
+
+    return np_data, np_labels
 
 
 if __name__ == "__main__":
