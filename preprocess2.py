b80aa82e11715c505a52857823c1ab034596af7a
Mikulas Bence <mikulasbence@gmail.com>
2016-11-10 01:25:37 +0100

    Preprocess2 added

---
+++ preprocess2.py
@@ -0,0 +1,47 @@
+import csv
+import os
+import gc
+
+import numpy as np
+from keras.preprocessing import image
+
+def csv_load(csv_file_name = "train_info_modified_fixed.csv"):
+    csv_data = {}
+
+    try:
+        with open(csv_file_name) as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader, None)
+            for line in reader:
+                csv_data[line[0]] = line[1:]
+    except Exception as e:
+        print(e)
+
+    return csv_data
+
+
+def load_images(csv_data, location = "train_sample"):
+    data = []
+    labels = []
+
+    for subdir, dirs, files in os.walk(location):
+        for file in files:
+            if file in csv_data:
+                img_loc = os.path.join(subdir, file)
+                img = image.load_img(img_loc, target_size=(256, 256))
+                labels.append(csv_data[file][0])
+                data.append(np.array(img))
+
+                if len(labels) % 2000 == 0:
+                    print(len(labels), "images loaded")
+
+    # gc.collect(); - nem volt sok értelme
+    # byte-ként is lehetne tárolni: np.array(data, dtype="uint8") - ennek sem volt sok értelme
+    return np.array(data), np.array(labels)
+
+
+if __name__ == "__main__":
+    print()
+
+    csv_data = csv_load()
+    images, labels = load_images(csv_data)
